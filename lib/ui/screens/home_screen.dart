6f82fe172d769e97e7291faf1ea5f92700ae3c14
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-05-05 22:31:52 +0600

    search input file added

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -9,30 +9,60 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-          elevation: 0,
-          title: Row(
-            children: [
-              Image.asset('assets/images/logo_nav.png'),
-              const Spacer(),
-              AppBarIconButton(
-                iconData: Icons.person,
-                onTap: () {},
+        elevation: 0,
+        title: Row(
+          children: [
+            Image.asset('assets/images/logo_nav.png'),
+            const Spacer(),
+            AppBarIconButton(
+              iconData: Icons.person,
+              onTap: () {},
+            ),
+            AppBarIconButton(
+              iconData: Icons.call,
+              onTap: () {},
+            ),
+            AppBarIconButton(
+              iconData: Icons.notifications,
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              keyboardType: TextInputType.text,
+              textInputAction: TextInputAction.search,
+              onSubmitted: (value) {},
+              decoration: InputDecoration(
+                hintStyle: const TextStyle(color: softGreyColor),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                fillColor: softGreyColor.withOpacity(0.1),
+                filled: true,
+                hintText: 'Search',
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: softGreyColor,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(8),
+                ),
               ),
-              AppBarIconButton(
-                iconData: Icons.call,
-                onTap: () {},
-              ),
-              AppBarIconButton(
-                iconData: Icons.notifications,
-                onTap: () {},
-              ),
-            ],
-          )),
-      body: Center(
-        child: Text('Home'),
+            ),
+
+          ],
+        ),
       ),
     );
   }
 }
-
